0da03f3896263d9536aff2388bae880d0aaa8f07
daniloeder <daniloeder1997@gmail.com>
2024-09-17 10:44:49 -0300

    updates

--- test.py
+++ test.py
@@ -118,27 +118,65 @@ def add_pricing(product):
             time.sleep(1)
         driver.execute_script("arguments[0].scrollIntoView();window.scrollBy(0, -50);", driver.find_element(By.CLASS_NAME, 'summary__price'))
         time.sleep(1)
-        price_block = driver.find_element(By.CLASS_NAME, 'summary__container')
+        price_block = driver.find_element(By.CLASS_NAME, 'summary__price')
         # button
-        price_block.find_element(By.CLASS_NAME, 'summary__price-fields').find_element(By.TAG_NAME, 'button').click()
-        # listbox__options
-        for option in price_block.find_elements(By.CLASS_NAME, 'listbox__option'):
-            if option.text == 'Buy It Now':
-                option.click()
-                break
+        try:
+            a = 0/0
+            button = price_block.find_element(By.CLASS_NAME, 'summary__price-fields').find_element(By.TAG_NAME, 'button')
+            button.click()
+        except:
+            for button in price_block.find_elements(By.TAG_NAME, 'button'):
+                if button.text == 'Auction':
+                    button.click()
+                    break
+        if button.text != 'Buy It Now':
+            # listbox__options
+            for option in price_block.find_elements(By.CLASS_NAME, 'listbox__option'):
+                if option.text == 'Buy It Now':
+                    option.click()
+                    break
         # get input with name 'price'
-        price_block.find_element(By.CLASS_NAME, 'summary__price-fields').find_element(By.TAG_NAME, 'button').click()
         while len(price_block.find_elements(By.NAME, 'price')) == 0:
             time.sleep(0.5)
         time.sleep(0.1)
         price_input = price_block.find_element(By.NAME, 'price')
         price_input.send_keys(str(product['price']))
     except Exception as e:
+        print("WRITING PRICING TO FILE")
+        write_to_file('pricing.html', price_block.get_attribute('outerHTML'))
         print(f"Error while adding pricing: {e}")
 
 def write_to_file(filename, data):
     with open(filename, 'w') as file:
         file.write(data)
+
+def shipping_policy():
+    try:
+        # summary__shipping
+        print("A")
+        shipping_block = driver.find_element(By.CLASS_NAME, 'summary__shipping')
+        # business-policy-field
+        print("B")
+        business_policy_field = shipping_block.find_element(By.CLASS_NAME, 'business-policy-field')
+        # business-policy-field__menu
+        print("C")
+        business_policy_field_menu = business_policy_field.find_element(By.CLASS_NAME, 'business-policy-field__menu')
+        print('Setting shipping policy... Click button')
+        button = business_policy_field_menu.find_element(By.CLASS_NAME, 'menu-button__button')
+        print("Clicking...")
+        button.click()
+        time.sleep(2)
+    except Exception as e:
+        print("Error:", e)
+    try:
+        print("Get shipping policy container")
+        if len(driver.find_elements(By.CLASS_NAME, 'lightbox-dialog__main')) > 0:
+            main_container = driver.find_element(By.CLASS_NAME, 'lightbox-dialog__main')
+            write_to_file('shipping_policy.html', main_container.get_attribute('outerHTML'))
+        else:
+            print("No lightbox-dialog__main found")
+    except Exception as e:
+        print("Error:", e)
 
 def add_shipping():
     # ADD SHIPPING
@@ -150,21 +188,22 @@ def add_shipping():
         try:
             print("Get summary__shipping--section (select_block)")
             select_block = shipping_block.find_element(By.CLASS_NAME, 'summary__shipping--section')
-            print("Get se-field--fluid")
-            select = select_block.find_element(By.CLASS_NAME, 'se-field--fluid')
-            print("Clicking...")
-            select.click()
-            time.sleep(0.1)
-            print("Get summary__shipping--field")
-            options = select_block.find_elements(By.CLASS_NAME, 'listbox__option')
-            print("Clicking... 2nd option")
-            options[1].click()
-            time.sleep(0.1)
-            print("Clicking select again...")
-            select.click()
+            if len(select_block.find_elements(By.CLASS_NAME, 'se-field--fluid')) > 0:
+                print("Get se-field--fluid")
+                select = select_block.find_element(By.CLASS_NAME, 'se-field--fluid')
+                print("Clicking...")
+                select.click()
+                time.sleep(0.1)
+                print("Get summary__shipping--field")
+                options = select_block.find_elements(By.CLASS_NAME, 'listbox__option')
+                print("Clicking... 2nd option")
+                options[1].click()
+                time.sleep(0.1)
+                print("Clicking select again...")
+                select.click()
         except Exception as e:
             print("WRITING SHIPPING TO FILE")
-            write_to_file('shipping.html', shipping_block.get_attribute('outerHTML'))
+            write_to_file('shipping2.html', shipping_block.get_attribute('outerHTML'))
             print("Error:", e)
             time.sleep(1)
         try:
@@ -173,7 +212,7 @@ def add_shipping():
             button.click()
         except Exception as e:
             print("Error:", e)
-
+        to_write = ''
         try:
             print("Get handlig-time block")
             handling_time_block = driver.find_element(By.CLASS_NAME, 'se-panel-container__body').find_element(By.TAG_NAME, 'div')
@@ -183,6 +222,7 @@ def add_shipping():
                     break
                 time.sleep(1)
             print("Get button")
+            to_write = driver.find_element(By.CLASS_NAME, 'se-panel-container__body').get_attribute('outerHTML')
             button = handling_time_block.find_element(By.TAG_NAME, 'button')
             print("Clicking...")
             for _ in range(10):
@@ -201,8 +241,9 @@ def add_shipping():
             driver.find_element(By.CLASS_NAME, 'textual-display').click()
             time.sleep(0.1)
         except Exception as e:
+            print("WRITING PREFECENCES TO FILE")
+            write_to_file('prefecences.html', to_write)
             print("Error", e)
-        
         try:
             prefecences_settings = driver.find_element(By.CLASS_NAME, 'se-panel-container__body')
             country_input = prefecences_settings.find_element(By.NAME, 'itemLocationCountry')
@@ -231,7 +272,6 @@ def add_shipping():
                 driver.find_element(By.CLASS_NAME, 'se-panel-container__header-suffix').find_element(By.TAG_NAME, 'button').click()
                 time.sleep(1)
         except Exception as e:
-            print("WRITING PREFECENCES TO FILE")
             write_to_file('prefecences.html', prefecences_settings.get_attribute('outerHTML'))
             print("Error:", e)
         try:
@@ -254,7 +294,18 @@ options.add_argument("--guest")
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
 
-products = [{'name': 'LR LIFETAKT Vita Active Daily Vitamin Drink Red Fruits', 'description': '<h2> LR LIFETAKT Vita Active Daily Vitamin Drink Red Fruits </h2><p>Vitamins for the whole family - 100 % vitamin supply with just one teaspoon.</p><p>A varied and balanced diet is an important part of a healthy lifestyle. This is because we can and must take in a variety of nutrients that the body needs every day with our food. To ensure that we are always well supplied, taking Vita Active can make a meaningful contribution. Vita Active is a concentrate of 21 natural fruits and vegetables that tastes delicious and provides the body with ten important vitamins. Vitamins are true artists: for example, vitamins D and B6 contribute to a normal function of the immune system, vitamin B12 has a function in cell division, and vitamin B1 contributes to a normal heart function.</p><br><h2> Ingredients/Nutritional information </h2><p><strong>Ingredients: </strong>Fruit and vegetable concentrates (94 %, consisting of red grapes, apples, sour cherries, elderberries, black currants, rose hips, strawberries, blackberries, carrots, sloes, blueberries, aronia berries, plums, red currants, sea buckthorn berries, lemons, peaches, apricots, raspberries, boysenberries), dextrose, tomato concentrate, emulsifier (sunflower lecithin), niacin, vitamin E, pantothenic acid, vitamin B12, biotin, vitamin D, vitamin B6, thiamine, riboflavin, folic acid.</p><p>*&nbsp;of the reference quantity for daily intake (NRV)</p><br><h2> Application </h2><p>Consume 5 ml once a day during or after a meal. You can also mix Vita Active as a spritzer with water, consume it with yoghurt, as a dessert or with muesli. Long-term consumption is recommended.</p><p>Do not exceed the recommended daily intake. Food supplements are not a substitute for a varied and balanced diet, which is important together with a healthy lifestyle. Keep product out of the reach of small children.</p><p>Do not store above 25 °C. Re-seal well after opening, store in the refrigerator.<br>Shake before use.</p><br><h2> Worth knowing </h2><p><strong>Did you know that vitamin D is also called the "sun vitamin"?</strong><br>It is a special vitamin that the body can produce under the influence of sunlight. A balanced diet normally supplies the body with all relevant vitamins. Especially in the dark season, Vita Active can be a useful addition to your diet.</p><p>A balanced diet is especially important for people who are very busy at work or with their families. Also pay attention to what you eat during such phases and supplement your diet with Vita Active if necessary.</p><br>', 'category': 'Nutrition Nutrient complex', 'brand': 'LR LIFETAKT', 'type': 'None', 'color': 'None', 'price': '26.39 GBP', 'images': ['https://cdn.lrworld.com/images_cms/images/product/884x1200/80301-50/lr_lifetakt_vita_active_daily_vitamin_drink_red_fruits.jpg'], 'filled': True, 'upc': '80301-50'}]
+#html = '<div class="additional-price-info shipping-costs">plus delivery costs<span class="inactive info tooltip btn-simple light" data-tooltip="<div class="title">Delivery costs</div><div class="content"><div></div><div></div><div>Shipping costs:</br> to £ 75.00 -> £ 9.99 </br> £ 75.01 to £ 150.00 -> £ 5.99 </br> above £ 150.00 -> free of charge</div></div>">i</span></div>'
+#shipping_costs_html = html.find_element(By.CLASS_NAME, 'shipping-costs').find_element(By.TAG_NAME, 'span').get_attribute('data-tooltip')
+#shipping_costs_html = "<div class='title'>Delivery costs</div><div class='content'><div></div><div></div><div>Shipping costs:</br> to £ 75.00 -> £ 9.99 </br> £ 75.01 to £ 150.00 -> £ 5.99 </br> above £ 150.00 -> free of charge</div></div>"
+#shipping_costs = shipping_costs_html.split('Shipping costs:')[1].split('</div>')[0].split('</br>')
+#shipping_costs = list(filter(None, shipping_costs))
+shipping_costs = [' to £ 75.00 -> £ 9.99 ', ' £ 75.01 to £ 150.00 -> £ 5.99 ', ' above £ 150.00 -> free of charge']
+
+
+
+products = [{
+    'shipping_policy': '',
+    'name': 'LR LIFETAKT Vita Active Daily Vitamin Drink Red Fruits', 'description': '<h2> LR LIFETAKT Vita Active Daily Vitamin Drink Red Fruits </h2><p>Vitamins for the whole family - 100 % vitamin supply with just one teaspoon.</p><p>A varied and balanced diet is an important part of a healthy lifestyle. This is because we can and must take in a variety of nutrients that the body needs every day with our food. To ensure that we are always well supplied, taking Vita Active can make a meaningful contribution. Vita Active is a concentrate of 21 natural fruits and vegetables that tastes delicious and provides the body with ten important vitamins. Vitamins are true artists: for example, vitamins D and B6 contribute to a normal function of the immune system, vitamin B12 has a function in cell division, and vitamin B1 contributes to a normal heart function.</p><br><h2> Ingredients/Nutritional information </h2><p><strong>Ingredients: </strong>Fruit and vegetable concentrates (94 %, consisting of red grapes, apples, sour cherries, elderberries, black currants, rose hips, strawberries, blackberries, carrots, sloes, blueberries, aronia berries, plums, red currants, sea buckthorn berries, lemons, peaches, apricots, raspberries, boysenberries), dextrose, tomato concentrate, emulsifier (sunflower lecithin), niacin, vitamin E, pantothenic acid, vitamin B12, biotin, vitamin D, vitamin B6, thiamine, riboflavin, folic acid.</p><p>*&nbsp;of the reference quantity for daily intake (NRV)</p><br><h2> Application </h2><p>Consume 5 ml once a day during or after a meal. You can also mix Vita Active as a spritzer with water, consume it with yoghurt, as a dessert or with muesli. Long-term consumption is recommended.</p><p>Do not exceed the recommended daily intake. Food supplements are not a substitute for a varied and balanced diet, which is important together with a healthy lifestyle. Keep product out of the reach of small children.</p><p>Do not store above 25 °C. Re-seal well after opening, store in the refrigerator.<br>Shake before use.</p><br><h2> Worth knowing </h2><p><strong>Did you know that vitamin D is also called the "sun vitamin"?</strong><br>It is a special vitamin that the body can produce under the influence of sunlight. A balanced diet normally supplies the body with all relevant vitamins. Especially in the dark season, Vita Active can be a useful addition to your diet.</p><p>A balanced diet is especially important for people who are very busy at work or with their families. Also pay attention to what you eat during such phases and supplement your diet with Vita Active if necessary.</p><br>', 'category': 'Nutrition Nutrient complex', 'brand': 'LR LIFETAKT', 'type': 'None', 'color': 'None', 'price': '26.39 GBP', 'images': ['https://cdn.lrworld.com/images_cms/images/product/884x1200/80301-50/lr_lifetakt_vita_active_daily_vitamin_drink_red_fruits.jpg'], 'filled': True, 'upc': '80301-50'}]
 
 while not ebay_login():
     pass
@@ -265,5 +316,7 @@ for product in products:
 driver.get('https://www.ebay.com/sl/prelist/suggest?sr=cubstart')
 
 list_products_in_ebay(products)
+shipping_policy()
+
 
 
