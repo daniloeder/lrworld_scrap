c86867057487a14b053889566315def8b71d5ed5
daniloeder <daniloeder1997@gmail.com>
2024-09-18 17:12:13 -0300

    Searching for pricing erros

--- test.py
+++ test.py
@@ -193,14 +193,20 @@ def add_pricing(product):
         time.sleep(0.1)
         price_input = price_block.find_element(By.NAME, 'price')
         price_input.send_keys(str(product['price']))
+        time.sleep(0.1)
         # add quantity
-        price_block.find_element(By.NAME, 'quantity').clear()
+        while price_block.find_element(By.NAME, 'quantity').get_attribute('value'):
+            price_block.find_element(By.NAME, 'quantity').send_keys('\b')
+        time.sleep(0.1)
         price_block.find_element(By.NAME, 'quantity').send_keys('15')
+        time.sleep(0.1)
         # disable best offer
         if len(price_block.find_elements(By.NAME, 'bestOfferEnabled')) > 0:
             price_block.find_element(By.NAME, 'bestOfferEnabled').click()
-    except Exception as e:
+        time.sleep(0.1)
         print("WRITING PRICING TO FILE")
+        write_to_file('pricing.html', price_block.get_attribute('outerHTML'))
+    except Exception as e:
         write_to_file('pricing.html', price_block.get_attribute('outerHTML'))
         print(f"Error while adding pricing: {e}")
 
